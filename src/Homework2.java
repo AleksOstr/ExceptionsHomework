2ea539b5e5960cedebebe02241f3276388732d1b
AleksOstr <ostryankoam@yandex.ru>
2023-04-16 19:47:23 +0400

    task2 done

--- src/Homework2.java
+++ src/Homework2.java
@@ -2,9 +2,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Homework2 {
-    public static void main(String[] args) {
-        float number = scanFloat();
-    }
     /* Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
     и возвращает введенное значение.
     Ввод текста вместо числа не должно приводить к падению приложения,
@@ -19,6 +16,16 @@ public class Homework2 {
         } catch (InputMismatchException e) {
             System.out.println("Only float numbers allowed!");
             return scanFloat();
+        }
+    }
+
+    // Задание 2
+    public static void divMethod(int[] intArray, int d) {
+        try {
+            int catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (NullPointerException | ArithmeticException | IndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e.getClass().getSimpleName());
         }
     }
 }
