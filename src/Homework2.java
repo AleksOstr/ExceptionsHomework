2363788d2af02162c66360e5c6b02597b7361456
AleksOstr <ostryankoam@yandex.ru>
2023-04-16 20:03:25 +0400

    task3 done

--- src/Homework2.java
+++ src/Homework2.java
@@ -28,4 +28,22 @@ public class Homework2 {
             System.out.println("Catching exception: " + e.getClass().getSimpleName());
         }
     }
+
+    // Задание 3
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }
+    }
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+
 }
