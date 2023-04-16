51d7a58f1857ded460f8e1099eb7812e36d9cccc
AleksOstr <ostryankoam@yandex.ru>
2023-04-16 19:37:20 +0400

    task1 done

--- src/Homework2.java
+++ src/Homework2.java
@@ -1,7 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Homework2 {
+    public static void main(String[] args) {
+        float number = scanFloat();
+    }
     /* Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
     и возвращает введенное значение.
     Ввод текста вместо числа не должно приводить к падению приложения,
     вместо этого, необходимо повторно запросить у пользователя ввод данных. */
-
+    public static float scanFloat() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter float number: ");
+            float inputFloat = scanner.nextFloat();
+            scanner.close();
+            return inputFloat;
+        } catch (InputMismatchException e) {
+            System.out.println("Only float numbers allowed!");
+            return scanFloat();
+        }
+    }
 }
