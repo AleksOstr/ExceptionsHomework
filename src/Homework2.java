ac8962592573815c674c77a56383e02c62358e28
AleksOstr <ostryankoam@yandex.ru>
2023-04-16 20:12:33 +0400

    task4 done

--- src/Homework2.java
+++ src/Homework2.java
@@ -2,6 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Homework2 {
+
     /* Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
     и возвращает введенное значение.
     Ввод текста вместо числа не должно приводить к падению приложения,
@@ -44,6 +45,23 @@ public class Homework2 {
     }
     public static void printSum(Integer a, Integer b) {
         System.out.println(a + b);
+    }
+
+    // Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+    // Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+    public static String inputString() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter your string:");
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                throw new NullPointerException();
+            } else return input;
+        } catch (NullPointerException e) {
+            System.out.println("Empty strings aren't allowed!");
+            return inputString();
+        }
     }
 
 }
